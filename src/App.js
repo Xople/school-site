561562c0ea977407d47efccff2786b240e29ccc1
Tito Shadam <titoshadampersonal@gmail.com>
2021-07-31 23:33:29 +0700

    add component container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import './style/main.scss';
+
+import Container from './components/include/Container';
+import {Layout, Menu, Row, Col} from 'antd';
+
+const {Header,Content} =  Layout; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header className="box-shadow-1" style={{ background: '#fff' }}>
+        <Row justify="end">
+          <Menu mode="horizontal" style={{ userSelect: 'none' }}>
+            <Menu.Item key="2">Menu 1</Menu.Item>
+            <Menu.Item key="3">Menu 1</Menu.Item>
+            <Menu.Item key="4">Menu 1</Menu.Item>
+          </Menu>
+        </Row>
+      </Header>
+
+      <Container>
+        <Content>
+          <Row gutter={16} justify="space-between">
+            <Col span={12}>
+              <h1>Hello World</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos itaque laborum aperiam sint nemo est dolorum iure perspiciatis ex adipisci.</p>
+            </Col>
+            <Col span={12}>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, ipsum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere adipisci corrupti maxime at tempore omnis quia beatae deleniti fugit architecto.</p>
+            </Col>
+          </Row>
+        </Content>
+      </Container>
     </div>
   );
 }
